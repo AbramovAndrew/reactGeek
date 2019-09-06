9d40748f4aaf4d45b9ef56e92f930da596d90374
Jim <j@j.com>
2019-09-07 01:28:59 +0300

    jQuery не нужна.

--- src/app/components/WelcomeModal.js
+++ src/app/components/WelcomeModal.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle';
 
 class WelcomeModal extends Component {
     constructor(props) {
