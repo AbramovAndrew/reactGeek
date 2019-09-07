f6c5f92e32f57231c4d56879a5a1078573d091a6
Jim <j@j.com>
2019-09-07 22:58:23 +0300

    Начало создания блога. Создание компонента Меню.

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+
+import Layout from "./app/layouts/Layout";
+import Main from "./app/pages/Main";
+import PageNotFound from "./app/pages/PageNotFound";
+
+ReactDOM.render(
+    <Router history={browserHistory}>
+        <Route path="/" component={Layout}>
+            <IndexRoute component={Main} />
+            <Route path="*" component={PageNotFound} />
+        </Route>
+    </Router>,
+    document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-    entry: "./src/App.js",
+    entry: "./src/index.js",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist")
@@ -19,5 +19,9 @@ module.exports = {
                 use: ["style-loader", "css-loader"]
             }
         ]
+    },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: path.resolve(__dirname, 'dist')
     }
 };
